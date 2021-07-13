e7d9abc91dd728112456a42816451f3fbe98323d
zmzimpl <461045745@qq.com>
2021-07-13 23:31:18 +0800

    回顾

--- tips/js/继承/组合继承/script.js
+++ tips/js/继承/组合继承/script.js
@@ -13,11 +13,11 @@ function Child(names) {
 Child.prototype = new Parent(); // 一次
 Child.prototype.constructor = Child;
 
+
 const c1 = new Child(); // 两次
 c1.getNames();
 const c2 = new Child(['Tom', 'Jim']);
 console.log(c2.names);
-
 
 // 缺点，会调用两次父构造函数,可能会创建了多余的属性
 
